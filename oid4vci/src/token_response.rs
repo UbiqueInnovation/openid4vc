6c162f999546e7199d79a78f8d94b4790590d923
Patrick Amrein <amrein@ubique.ch>
2025-05-14 12:24:46 +0200

    allow string in response (lenient parsing)

--- oid4vci/src/token_response.rs
+++ oid4vci/src/token_response.rs
@@ -7,10 +7,25 @@ use serde_with::skip_serializing_none;
 pub struct TokenResponse {
     pub access_token: String,
     pub token_type: String,
-    pub expires_in: Option<u64>,
+    pub expires_in: Option<StringOrInt>,
     pub refresh_token: Option<String>,
     pub scope: Option<String>,
     pub c_nonce: Option<String>,
-    pub c_nonce_expires_in: Option<u64>,
+    pub c_nonce_expires_in: Option<StringOrInt>,
     // TODO: add `authorization_details` field when support for Authorization Code Flow is added.
+}
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+#[serde(untagged)]
+pub enum StringOrInt {
+    String(String),
+    Int(u64),
+}
+
+impl From<StringOrInt> for u64 {
+    fn from(value: StringOrInt) -> Self {
+        match value {
+            StringOrInt::String(s) => u64::from_str_radix(s.as_str(), 10).unwrap_or(0),
+            StringOrInt::Int(i) => i,
+        }
+    }
 }
